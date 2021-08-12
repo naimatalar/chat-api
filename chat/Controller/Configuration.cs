485cbf33303ce44a89a944d1c3517aa1dead1303
naimatalar <istemi1016@gmail.com>
2021-08-12 18:14:46 +0300

    jghfjkgh

--- chat/Controller/Configuration.cs
+++ chat/Controller/Configuration.cs
@@ -19,25 +19,51 @@ namespace chat.Controller
             _context = context;
         }
 
+        [HttpGet("GetAllWebSite")]
+        public ActionResult<dynamic> getWebSite()
+        {
+            var d = _context.WebSites.Select(x => new
+            {
+                x.CreateDate,
+                LastMessageDate = x.MessageTopics.FirstOrDefault() == null ? "-" : x.MessageTopics.FirstOrDefault().CreateDate.ToString("dd-MMM-yyyy hh:mm"),
+                x.Id,
+                x.WebSiteName,
+                MessageTopicsCount = x.MessageTopics.Count(),
+
+            }).ToList();
+            return d;
+        }
+        [HttpGet("GetAllWebSiteById/{Id}")]
+        public ActionResult<dynamic> getWebSiteById(Guid Id)
+        {
+            var d = _context.WebSites.Where(x => x.Id == Id).Select(x => new
+            {
+                x.CreateDate,
+                x.Id,
+                x.WebSiteName,
+
+            }).ToList();
+            return d;
+        }
         [HttpPost("CreateEditWebSite")]
         public ActionResult<dynamic> GetToken(CreateEditWebSiteRequestModel model)
         {
-            if (model.Id==null)
+            if (model.Id == null)
             {
                 var data = new Entites.WebSites
                 {
                     WebSiteName = model.Name
 
                 };
-                _context.WebSites.Add(data) ;
+                _context.WebSites.Add(data);
                 return data;
             }
-            var rdata = _context.WebSites.Where(x => x.Id == model.Id ).FirstOrDefault();
+            var rdata = _context.WebSites.Where(x => x.Id == model.Id).FirstOrDefault();
             if (rdata == null)
             {
                 BadRequest("Kayıt Bulunamadı");
             }
-            rdata.WebSiteName=model.Name;
+            rdata.WebSiteName = model.Name;
             _context.Entry(rdata).State = EntityState.Modified;
 
             _context.SaveChanges();
@@ -46,7 +72,7 @@ namespace chat.Controller
         [HttpGet("DeleteWebSite/{id}")]
         public ActionResult<dynamic> DeleteWebSite(Guid Id)
         {
-         
+
             var rdata = _context.WebSites.Where(x => x.Id == Id).FirstOrDefault();
             if (rdata == null)
             {
@@ -56,6 +82,35 @@ namespace chat.Controller
             _context.SaveChanges();
             return rdata;
         }
+        [HttpGet("GetAllUser")]
+        public ActionResult<dynamic> getUsers()
+        {
+            var d = _context.Users.Select(x => new
+            {
+                x.CreateDate,
+                x.Id,
+                x.Name,
+                x.Mail,
+                x.Password
+
+            }).ToList();
+            return d;
+        }
+        [HttpGet("GetUserById/{id}")]
+        public ActionResult<dynamic> getUsersById(Guid Id)
+        {
+            var d = _context.Users.Where(x => x.Id == Id).Select(x => new
+            {
+                x.CreateDate,
+                x.Id,
+                x.Name,
+                x.Mail,
+                x.Password
+
+            }).FirstOrDefault();
+            return d;
+        }
+
         [HttpPost("CreateEditUser")]
         public ActionResult<dynamic> CreateUser(CreateEditUserRequestModel model)
         {
@@ -64,11 +119,12 @@ namespace chat.Controller
                 var data = new Entites.User
                 {
                     Name = model.Name,
-                    Mail=model.Mail,
-                    Password=model.Password,
+                    Mail = model.Mail,
+                    Password = model.Password,
 
                 };
                 _context.Users.Add(data);
+                _context.SaveChanges();
                 return data;
             }
             var rdata = _context.Users.Where(x => x.Id == model.Id).FirstOrDefault();
@@ -86,7 +142,7 @@ namespace chat.Controller
             _context.SaveChanges();
             return rdata;
         }
-        [HttpGet("DeleteUSer/{id}")]
+        [HttpGet("DeleteUser/{id}")]
         public ActionResult<dynamic> DeleteUser(Guid Id)
         {
 
@@ -99,7 +155,20 @@ namespace chat.Controller
             _context.SaveChanges();
             return rdata;
         }
+        [HttpGet("GetAllTopic/{id}")]
+        public ActionResult<dynamic> getTopic(Guid id)
+        {
+            var d = _context.MessageTopics.Where(x=>x.WebSiteId==id).Select(x => new
+            {
+                WebSiteName = x.WebSite.WebSiteName,
+                WebSiteId = x.WebSite.Id,
+                x.RiderMail,
+                x.RiderName,
 
+
+            }).ToList();
+            return d;
+        }
 
     }
 
@@ -107,7 +176,8 @@ namespace chat.Controller
 
 
 
-    public class CreateEditWebSiteRequestModel {
+    public class CreateEditWebSiteRequestModel
+    {
         public Guid? Id { get; set; }
         public string Name { get; set; }
 
